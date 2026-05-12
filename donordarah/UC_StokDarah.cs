a50e6cfba76f2f8696c67828fa146c3858788389
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2026-05-13 00:45:22 +0700

    initial commit user control stokdarah

---
+++ donordarah/UC_StokDarah.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace donordarah
+{
+    public partial class UC_StokDarah : UserControl
+    {
+        // Gunakan connection string milikmu
+        string connString = @"Data source=DESKTOP-E32H1C2\BINTANGAF;initial catalog=DBdonordarah;integrated security=True;TrustServerCertificate=True";
+
+        public UC_StokDarah()
+        {
+            InitializeComponent();
+        }
+
+      
