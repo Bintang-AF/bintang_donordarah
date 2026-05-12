b955df75253b668995424f6ba6dc909e728252c3
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2026-05-13 00:42:34 +0700

    membuat user control transaksi

---
+++ donordarah/UC_Transaksi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace donordarah
+{
+    public partial class UC_Transaksi : UserControl
+    {
+        string connString = @"Data source=DESKTOP-E32H1C2\BINTANGAF;initial catalog=DBdonordarah;integrated security=True;TrustServerCertificate=True";
+
+        public UC_Transaksi()
+        {
+            InitializeComponent();
+        }
+
+        // 1. EVENT SAAT HALAMAN DIBUKA
+      
