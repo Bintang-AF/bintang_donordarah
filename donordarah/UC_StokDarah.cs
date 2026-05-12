74052b7cc4455536290ed5d7324d358000ac7a64
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2026-05-13 00:45:55 +0700

    lanjutan uc stokdarah 2

--- donordarah/UC_StokDarah.cs
+++ donordarah/UC_StokDarah.cs
@@ -15,4 +15,38 @@ namespace donordarah
             InitializeComponent();
         }
 
-      
+        private void UC_StokDarah_Load(object sender, EventArgs e)
+        {
+            // Set default pilihan filter agar tidak kosong
+            cbFilterGoldar.SelectedIndex = 0; // "Semua"
+            cbFilterStatus.SelectedIndex = 0; // "Semua"
+
+            LoadRingkasanStok();
+            LoadDataStok();
+        }
+
+        // FUNGSI MENGAMBIL ANGKA RINGKASAN (GOL A, B, AB, O)
+        private void LoadRingkasanStok()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetRingkasanStok", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        lblStokA.Text = dr["StokA"].ToString() + " Kantong";
+                        lblStokB.Text = dr["StokB"].ToString() + " Kantong";
+                        lblStokAB.Text = dr["StokAB"].ToString() + " Kantong";
+                        lblStokO.Text = dr["StokO"].ToString() + " Kantong";
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show("Gagal Ringkasan: " + ex.Message); }
+            }
+        }
+
+        // FUNGSI MENAMPILKAN TABEL (MENGGUNAKAN VIEW & SP)
+       
