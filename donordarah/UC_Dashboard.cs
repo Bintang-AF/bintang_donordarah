d304651ed6569dcbf8634b9d86c2b9ef2722fa6b
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2026-05-13 00:39:13 +0700

    membuat user control dashboard

---
+++ donordarah/UC_Dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace donordarah
+{
+    public partial class UC_Dashboard : UserControl
+    {
+        public UC_Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        // Event saat halaman dashboard dimuat
+        private void UC_Dashboard_Load(object sender, EventArgs e)
+        {
+            HitungDashboard();
+        }
+
+     
